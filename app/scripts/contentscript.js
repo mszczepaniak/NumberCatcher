4bab3232b94ac34cfcfa638a3d0c31d5ace36e40
Michal S <mr.szczepaniak@gmail.com>
2015-04-21 22:14:40 +0200

    Hook the controler to the DOM element with ID

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -7,5 +7,10 @@ window.addEventListener("load", function() {
 	html.setAttribute('ng-app', '');
 	html.setAttribute('ng-csp', '');
 
+	var viewport = document.getElementById('viewport');
+	viewport.setAttribute('ng-controller', 'MainController');
+	app.controller('MainController', function ($scope) {});
+
 	angular.bootstrap(html, ['Binged'], []);
 });
+
